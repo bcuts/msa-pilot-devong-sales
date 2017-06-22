5588c832655b33bc046cde80433fa1267bb53a34
Joonhyeok Im <itanoss@gmail.com>
2017-06-22 14:44:54 +0900

    Resolve CORS issue.
    - Temporarily all client ips are allowed.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from database import db, Purchase
 
 app = Flask(__name__)
+CORS(app)       # TODO Avoid this later!
 
 
 @app.route('/')
