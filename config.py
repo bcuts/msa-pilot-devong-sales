18596ae84c2bdbd06c97b9a36e5308f9c60ec328
Joonhyeok Im <itanoss@gmail.com>
2017-06-27 10:30:03 +0900

    Fix environment variable gathering part

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-
-import app
+import os
 
 ENVVAR_KEY_API_ENDPOINT_BRANCH = 'API_ENDPOINT_BRANCH'
 
@@ -7,8 +6,4 @@ DEFAULT_API_ENDPOINT_BRANCH = 'http://localhost:8080'
 
 
 def get_branch_api_endpoint():
-    try:
-        return app.app.config.from_envvar(ENVVAR_KEY_API_ENDPOINT_BRANCH)
-    except RuntimeError as e:
-        print('WARNING: API_ENDPOINT_BRANCH loading failed, details:', e)
-        return DEFAULT_API_ENDPOINT_BRANCH
+    return os.getenv(ENVVAR_KEY_API_ENDPOINT_BRANCH, DEFAULT_API_ENDPOINT_BRANCH)
