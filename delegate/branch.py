e1bbb91d1320f4e4d0e3872989b392b72c432867
Joonhyeok Im <itanoss@gmail.com>
2017-06-27 10:50:26 +0900

    Enhance logging in branch delegation part

--- delegate/branch.py
+++ delegate/branch.py
@@ -2,8 +2,9 @@ import requests
 
 
 def get(branch_id, endpoint):
-    r = requests.get(endpoint + '/branch/{}'.format(branch_id))
+    url = endpoint + '/branch/{}'.format(branch_id)
+    r = requests.get(url)
     if r.status_code == 200:
         return r.json() if r.content else None
 
-    raise "Request wrong, status_code={}".format(r.status_code)
+    raise "Request wrong to {}, status_code={}".format(url, r.status_code)
